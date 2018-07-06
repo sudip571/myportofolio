44a5b032631860867e7afde9d3abc3dcbeb8d394
Sudip Ranabhat <sranabhat@resolutionmedia.com>
2018-07-06 18:28:15 +0545

    Changes made on runserver file

--- runserver.py
+++ runserver.py
@@ -10,8 +10,11 @@ config_name=os.environ.get('FLASK_CONFIG','default')
 app = create_app(config_name)
  
 if __name__ == '__main__':
-    print(app.config['EMAIL_RECEIVER'])
-    print(app.config['TOP_LEVEL_DIR'])
-     
-    app.run()
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
+    
     
